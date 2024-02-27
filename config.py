f6b8078810dab4ea85e19cb0459c58cf4cb4016d
Luis <luisftomasprado@gmail.com>
2024-02-27 17:05:27 -0300

    feat: Preparing the enviroment and the settings for the project

---
+++ config.py
@@ -0,0 +1,9 @@
+from decouple import config
+
+
+class Config:
+    MYSQL_HOST = config("MYSQL_HOST")
+    MYSQL_PORT = config("MYSQL_PORT", cast=int)
+    MYSQL_USER = config("MYSQL_USER")
+    MYSQL_PASSWORD = config("MYSQL_PASSWORD")
+    MYSQL_DB = config("MYSQL_DB")
