79bafb89500c812fe701c522b7666aec9c7a18c8
Luis <luisftomasprado@gmail.com>
2024-02-27 17:40:03 -0300

    fix: Updating the MYSQL_PORT settings

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from decouple import config
 
 class Config:
     MYSQL_HOST = config("MYSQL_HOST")
-    MYSQL_PORT = config("MYSQL_PORT", cast=int)
+    MYSQL_PORT = config("MYSQL_PORT", default=3306, cast=int)
     MYSQL_USER = config("MYSQL_USER")
     MYSQL_PASSWORD = config("MYSQL_PASSWORD")
     MYSQL_DB = config("MYSQL_DB")
