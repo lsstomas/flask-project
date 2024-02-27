016ac40b9b636175adca1d7044e08dac04090dbd
Luis <luisftomasprado@gmail.com>
2024-02-27 18:07:13 -0300

    feat: Implementing exception handling in the 'models.py' and a method for creating products

--- src/models.py
+++ src/models.py
@@ -1,9 +1,10 @@
 import mysql.connector
+from mysql.connector import Error
 from config import Config
 
 
 class Produto:
-    def __init__(self, id, nome, descricao, preco):
+    def __init__(self, nome, descricao, preco, id=None):
         self.id = id
         self.nome = nome
         self.descricao = descricao
@@ -12,25 +13,47 @@ class Produto:
     # Método para conectar ao banco de dados
     @staticmethod
     def get_db_connection():
-        return mysql.connector.connect(
-            host=Config.MYSQL_HOST,
-            port=Config.MYSQL_PORT,
-            user=Config.MYSQL_USER,
-            password=Config.MYSQL_PASSWORD,
-            database=Config.MYSQL_DB,
-        )
+        try:
+            connection = mysql.connector.connect(
+                host=Config.MYSQL_HOST,
+                user=Config.MYSQL_USER,
+                password=Config.MYSQL_PASSWORD,
+                database=Config.MYSQL_DB,
+                port=Config.MYSQL_PORT,
+            )
+            return connection
+        except Error as e:
+            print(f"Erro ao estabelecer conexão com a plataforma MySQL: {e}")
+            return None
 
     # Método para listar todos produtos
     @staticmethod
     def get_all():
         connection = Produto.get_db_connection()
 
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM produto")
+        if connection is not None:
+            cursor = connection.cursor(dictionary=True)
 
-        produtos = cursor.fetchall()
+            cursor.execute("SELECT * FROM produto")
+            produtos = cursor.fetchall()
 
-        cursor.close()
-        connection.close()
+            cursor.close()
+            connection.close()
 
-        return produtos
+            return produtos
+        return []
+
+    # Método para adicionar produtos
+    def add_product(self):
+        connection = Produto.get_db_connection()
+
+        if connection is not None:
+            cursor = connection.cursor()
+
+            cursor.execute(
+                f'INSERT INTO produto (nome, descricao, preco) VALUES ("{self.nome}", "{self.descricao}", {self.preco})'
+            )
+
+            connection.commit()
+            cursor.close()
+            connection.close()
